92aa108da017a959667b5761ee465ceda7310c5c
Samudra Mazumder <samudramazumder@gmail.com>
2023-09-09 15:10:02 +0600

    added one question and answer

--- Projects/QuestionsAns.java
+++ Projects/QuestionsAns.java
@@ -11,6 +11,8 @@ public class QuestionsAns {
         answer.add("Paris");
         questions.add("What is the capital of Bangladesh?");
         answer.add("Dhaka");
+        questions.add("What is the capital of India?");
+        answer.add("Delhi");
         hangman.add("\n+- - - - - -+\n");
         hangman.add("      |   |\n");
         hangman.add("      O   |\n");
