88b271cb340a316eca3c4a70b87e8f7a12f89fd8
shuvo2k23 <rshuvo036@gmail.com>
2023-09-10 21:13:29 +0600

    version 2.0.1

--- Projects/QuestionsAns.java
+++ Projects/QuestionsAns.java
@@ -13,12 +13,28 @@ public class QuestionsAns {
         answer.add("Dhaka");
         questions.add("What is the capital of India?");
         answer.add("Delhi");
-        questions.add("Who is the best all rounder in cricket?");
+        questions.add("Who is the best all rounder of cricket?");
         answer.add("Shakib");
-        questions.add("Who is the best player in football?");
+        questions.add("Who is the best player of football?");
         answer.add("Neymar");
         questions.add("Which team won the FIFA World Cup five times?");
         answer.add("Brazil");
+        questions.add("Which team was winner of last FIFA world cup??");
+        answer.add("Argentina");
+        questions.add("What is the name of largest bridge of Bangladesh?");
+        answer.add("Padmabridge");
+        questions.add("What is the name of Bangladesh national Bird?");
+        answer.add("Magpie");
+        questions.add("Which year bangladesh was independent?");
+        answer.add("1971");
+        questions.add("Which city is called \"SilkCity\" in Bangladesh?");
+        answer.add("Rajshahi");
+        questions.add("When Rajshahi University was established?");
+        answer.add("1953");
+
+
+
+        //hangman------------------------------------------------------
         hangman.add("\n+- - - - - -+\n");
         hangman.add("      |   |\n");
         hangman.add("      O   |\n");
